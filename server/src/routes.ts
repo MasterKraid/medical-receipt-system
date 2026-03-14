d5d38f984c9f9608ff1540f0fdbf6c079ab7eae5
Kraid-AsifServer <72755775+MasterKraid@users.noreply.github.com>
2026-03-14 17:17:15 +0530

    Migrate: TO Bcryptjs FROM Bcrypt

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import path from 'path';
 import fs from 'fs';
 import multer from 'multer';
